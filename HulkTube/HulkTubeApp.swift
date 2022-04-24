047ec3e804d287ee16005f97847edcde494d4971
admirsaheta <81534875+admirsaheta@users.noreply.github.com>
2022-04-24 21:09:58 +0200

    Initial Commit

---
+++ HulkTube/HulkTubeApp.swift
@@ -0,0 +1,20 @@
+//
+//  HulkTubeApp.swift
+//  HulkTube
+//
+//  Created by admin on 24.4.22..
+//
+
+import SwiftUI
+
+@main
+struct HulkTubeApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
